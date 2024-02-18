486f8e6b57ea25cfe78f7baf73e378f848e5990a
DiegoMhz <dibzz456@gmail.com>
2024-02-18 13:34:59 -0400

    Prueba

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express')
 const app = express();
 const multer = require('multer');
-const sharp = require('sharp');
 const mongoose = require('mongoose');
 const patch = require('path');
 const cors = require('cors');
